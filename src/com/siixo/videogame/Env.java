c0727dbe0f8fe6d5f88b9cef5976ea8befa56f0a
Siixo <nicolas.knittel@proton.me>
2025-12-16 15:02:29 +0100

    Removed Env.java from tracking oops

--- src/com/siixo/videogame/Env.java
+++
@@ -1,7 +0,0 @@
-package com.siixo.videogame;
-
-public class Env {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/videogame";
-    public static final String DB_USER = "siixo";
-    public static final String DB_PASSWORD = "siixo";
-}
